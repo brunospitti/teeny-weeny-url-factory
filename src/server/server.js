0f60be59b6b2f6b975e8cf047c8a6caad2111dc2
Bruno Spitti <bruno.olivares@live.com>
2020-12-15 11:22:13 +0000

    Trying to use netlify

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
+import serverless from 'serverless-http';
 import { APIRouter } from './router/APIRouter';
 import { appRouter } from './router/appRouter';
 
@@ -10,6 +11,7 @@ require('dotenv').config();
 require('./database/connect');
 
 const app = express();
+const router = express.Router();
 
 app.set('port', process.env.PORT || 5555);
 app.set('host', process.env.APP_HOST || 'localhost');
@@ -19,6 +21,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../../dist')));
+
+app.use('/.netlify/functions/server', router); // path must route to lambda
 
 app.use('/api', APIRouter);
 
@@ -34,6 +38,11 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(app.get('port'), () =>
-  console.log(`Listening to ${app.get('host')} on port ${app.get('port')}`)
-);
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(app.get('port'), () =>
+    console.log(`Listening to ${app.get('host')} on port ${app.get('port')}`)
+  );
+}
+
+module.exports = app;
+module.exports.handler = serverless(app);
